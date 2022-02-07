85dfede725e9521e731935e7386114820d4a09ee
Saverio Tassinari <saverio.tassinari@gmail.com>
2022-02-07 21:02:58 +0000

    refactored example styles to component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import { css } from "@emotion/react";
 import { useState } from "react";
-import tw from "twin.macro";
 import "./App.css";
 import logo from "./logo.svg";
+import StylesExample from "./StylesExample";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,18 +11,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
-        <p
-          css={css`
-            color: green;
-          `}
-        >
-          This is a run-of-the-mill Emotion style 👩‍🎤
-        </p>
-        <p tw="font-bold">This is a Twin-macro example 💅</p>
-        <p css={[tw`text-yellow-300`, count % 2 === 0 && tw`text-red-700`]}>
-          This is a crazy conditional style with Emotion + Twin.macro
-        </p>
-        <p tw="text-sm">(It which changes when you increase the counter! 🤫)</p>
+
+        <StylesExample count={count} />
+
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
