da728afb179483d8234ccf87a1656703d2e1dcae
Saverio Tassinari <saverio.tassinari@gmail.com>
2022-02-07 20:32:41 +0000

    added emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { css } from "@emotion/react";
+import { useState } from "react";
+import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
+        <p
+          css={css`
+            color: green;
+          `}
+        >
+          This is a run-of-the-mill Emotion style
+        </p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
@@ -27,7 +35,7 @@ function App() {
           >
             Learn React
           </a>
-          {' | '}
+          {" | "}
           <a
             className="App-link"
             href="https://vitejs.dev/guide/features.html"
@@ -39,7 +47,7 @@ function App() {
         </p>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
