5d2fcb18c413d4bf88d47ca1bbe1374f83ab4f49
Saverio Tassinari <saverio.tassinari@gmail.com>
2022-02-27 02:00:56 +0000

    first pass at routes

---
+++ src/DemoThing.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import "./DemoThing.css";
+import logo from "./logo.svg";
+import StylesExample from "./StylesExample";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+
+        <StylesExample count={count} />
+
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {" | "}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
