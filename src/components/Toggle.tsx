0a9591756a59911f3e5907cccf712733c32d5775
Saverio Tassinari <saverio.tassinari@gmail.com>
2022-04-02 22:52:10 +0100

    new toggle component, power

---
+++ src/components/Toggle.tsx
@@ -0,0 +1,45 @@
+import React, { ReactNode } from "react";
+import tw from "twin.macro";
+
+interface ToggleContainerProps {
+  children: ReactNode;
+  isOn: boolean;
+}
+
+export const ToggleContainer: React.FC<ToggleContainerProps> = ({
+  children,
+  isOn,
+}) => (
+  <div
+    css={[
+      tw`relative flex justify-between w-full h-full gap-2 before:(content transition-all duration-300 absolute rounded-md width[calc(50% - 4px)] h-full dark:bg-darkish-grey light:bg-off-white)`,
+      isOn ? tw`before:left-0` : tw`before:left[calc(50% + 4px)]`,
+    ]}
+  >
+    {children}
+  </div>
+);
+
+interface ToggleButtonProps {
+  label: string;
+  selected: boolean;
+  onClick: () => void;
+}
+
+export const ToggleButton: React.FC<ToggleButtonProps> = ({
+  label,
+  selected,
+  onClick,
+}) => (
+  <button
+    onClick={onClick}
+    css={[
+      tw`z-10 flex items-center justify-center w-full h-full text-normal`,
+      selected
+        ? tw` dark:text-lighter-grey light:text-darker-grey`
+        : tw`dark:text-medium-grey light:text-light-grey`,
+    ]}
+  >
+    {label}
+  </button>
+);
--- src/pages/Settings/PowerToggle.tsx
+++ src/pages/Settings/PowerToggle.tsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import tw from "twin.macro";
 import BlockLabel from "../../components/BlockLabel";
+import { ToggleButton, ToggleContainer } from "../../components/Toggle";
 import { blockStyles } from "./Settings";
 
 export const PowerToggle = () => {
+  const [power, setPower] = useState<"on" | "off">("off");
   return (
     <article>
       <BlockLabel>Power</BlockLabel>
-      <div css={[blockStyles, tw`p-3`]}>lulz</div>
+      <div css={[blockStyles, tw`p-3`]}>
+        <ToggleContainer isOn={power === "on"}>
+          <ToggleButton
+            label="On"
+            selected={power === "on"}
+            onClick={() => setPower("on")}
+          />
+          <ToggleButton
+            label="Sleep"
+            selected={power === "off"}
+            onClick={() => setPower("off")}
+          />
+        </ToggleContainer>
+      </div>
     </article>
   );
 };
