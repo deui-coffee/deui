41338c3b94b43afa6adc9f073112e27d7b594dd6
Mariusz Roliński <mariusz@mondoreale.pl>
2022-04-26 23:56:20 +0200

    Format

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,30 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react({
-      jsxImportSource: "@emotion/react",
+      jsxImportSource: '@emotion/react',
       babel: {
         plugins: [
-          "babel-plugin-macros",
-          "@emotion/babel-plugin",
+          'babel-plugin-macros',
+          '@emotion/babel-plugin',
           [
-            "@emotion/babel-plugin-jsx-pragmatic",
+            '@emotion/babel-plugin-jsx-pragmatic',
             {
-              export: "jsx",
-              import: "__cssprop",
-              module: "@emotion/react",
+              export: 'jsx',
+              import: '__cssprop',
+              module: '@emotion/react',
             },
           ],
           [
-            "@babel/plugin-transform-react-jsx",
-            { pragma: "__cssprop" },
-            "twin.macro",
+            '@babel/plugin-transform-react-jsx',
+            { pragma: '__cssprop' },
+            'twin.macro',
           ],
         ],
       },
     }),
   ],
-});
+})
