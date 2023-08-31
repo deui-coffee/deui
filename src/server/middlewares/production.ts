ae9cfd1046c05d090a25c4089dae8b42888540c0
Mariusz Roliński <mariusz@mondoreale.pl>
2023-08-31 16:12:16 +0200

    Default to production env

--- src/server/middlewares/production.ts
+++ src/server/middlewares/production.ts
@@ -1,9 +1,10 @@
 import { NextFunction, Request, Response } from 'express'
 import path from 'path'
 import serveStatic from 'serve-static'
+import env from '../env'
 
 export default function production() {
-    if (process.env.NODE_ENV !== 'production') {
+    if (env !== 'production') {
         return (_: Request, __: Response, next: NextFunction) => {
             next()
         }
